0a4b6fffe899e845839ff1bc69d55369427b4bb9
Mohammad Saeed <msaeedsaeedi@outlook.com>
2024-07-02 15:14:55 +0500

    (feat): Initialize H2DE Engine with .cfg

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+#include <H2DE/engine.hpp>
+#include <libconfig.h++>
 int main() {
+    H2DE::Engine engine;
+    engine.init("assets/configurations/config.cfg");
+    engine.run();
     return 0;
 }
